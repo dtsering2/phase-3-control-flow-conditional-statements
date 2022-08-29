9590a89adcc968907dde26aa60df68a4534548d3
Tsering Tsering <dtsering2@gmail.com>
2022-08-29 16:40:11 -0400

    commit tree 36fe3941f37a495d685455a9ef53e81f67104331

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,53 @@
+
 def admin_login(username, password)
-  # your code here
+  if username == "admin" && password == "12345" || username == "ADMIN" && password == "12345"
+    "Access granted"
+  else 
+    "Access denied"
+  end
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when 40..65
+    "It's a little chilly out there!" 
+  else 
+    if temperature > 85
+      "It's too dang hot out there!"
+    elsif temperature < 40
+      "It's brisk out there!"
+    else 
+      "It's perfect out there!"
+    end
+  end
 end
 
+
+
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0 && num % 5 != 0
+    return "Fizz"
+  elsif num % 3 != 0 && num % 5 == 0
+    return "Buzz"
+  else
+    return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+    when "+"
+      num1 + num2
+    when "-"
+      num1-num2
+    when "*"
+      num1*num2
+    when "/"
+      num1/num2
+    else
+      puts "Invalid operation!"
+    end
 end
-
